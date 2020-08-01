5df2c48148e57a5deb70217dad2ea2fcbfc35337
jkosten <jkosten@cs.washington.edu>
2020-08-01 12:31:30 -0700

    updated stock sim. Need to fix shock and drift

--- stock_sim.py
+++ stock_sim.py
@@ -5,9 +5,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pandas_datareader import data 
 
-apple = data.DataReader('AAPL', 'yahoo', start='1/1/2009')
+def getData(stockSym:str, startDate:str, endDate:str):
+    stock = data.DataReader(stockSym, 'yahoo', start=startDate, end=endDate)
+    return stock
 
-apple.head()
+stock = getData(stockSym='AAPL',startDate='1/10/2020',endDate='7/1/2020')
 
-time_elapsed = (apple.index[-1] - apple.index[0]).days
+def predict(ntrials:int = 1000, predictionWindow:int = 21):
+    timeElapsed = (stock.index[-1] - stock.index[0]).days
+    mean = np.mean(stock, axis=0)[3]
+    print(mean)
+    std = np.std(stock,axis=0)[3]
+    mostRecentPrice = stock['Close'][-1]
+    print(std)
+    #Drift is the general movment of a stock over time
+    drift = (mean - (std**2)/2)
+    #Schock represents the random volatility of a stock's price
+    shock = std * np.random.rand()
+    
+    predictions = np.zeros((ntrials,predictionWindow))
+    #Set first element of array as most recent price of the given period
+    for i in range(ntrials):
+        predictions[i][0] = mostRecentPrice
+ 
 
+
+    for i in range(10):
+        for j in range(predictionWindow - 1):
+            predictions[i][j + 1] = (predictions[i][j] * np.exp(drift + shock))
+            print(predictions)
+    return predictions
+
+# predictions = predict()
+# plt.plot(predictions)
+# plt.savefig('predictions2')
+# plt.show()
